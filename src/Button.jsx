b0c7f11a06034d54927e608e562e7ffa8e49eefc
Steven Calderon <steven_cratus@hotmail.com>
2022-11-24 00:34:54 -0500

    ProTypes and DefaultProtypes

---
+++ src/Button.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+//Primera  forma de poner por defecto un valor a una variable
+export function Button({ text, category = "Uncategorized" }) {
+  if (!text) console.error("El texto es requerido");
+  return (
+    <button>
+      {text} - {category}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+//Segunda forma de poner por defecto valores
+Button.defaultProps = {
+  category: "Unknow",
+};
--- src/index.js
+++ src/index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client"; //Necesito react dom desde lado del cliente.
 import { Greeting, UserCard } from "./Greeting"; //importando el componente
 import Product, { Navbar } from "./Product";
+import { Button } from "./Button";
+import { Task } from "./Task";
 
 const rootElement = document.getElementById("root"); // obtengo el elemento "id:root" del index.html
 const root = ReactDOM.createRoot(rootElement); // Es un elemento que React lo usa bastante es el elemento raiz
 
 root.render(
   <div>
-    <UserCard
+    <Button text="click me" />
+    <Button text="Don't Click me" />
+    <Button text="" category="Action" />
+
+    {/* <UserCard
       name="Ryan Ray"
       amount={3000}
       married={true}
@@ -33,6 +39,6 @@ root.render(
       married={true}
       points={[99, 33.3, 22.2]}
       address={{ street: "123 Inca Street", city: "Pe York" }}
-    />
+    /> */}
   </div>
 );
