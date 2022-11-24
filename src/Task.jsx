b30cbc6c4a1949cb56874de214ebbf2ee52550e3
Steven Calderon <steven_cratus@hotmail.com>
2022-11-24 17:49:31 -0500

    Styles inline and import .css

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,6 +1,11 @@
-export function TaskCard () {
-    return <div>
-        <h1>Mi primer tarea</h1>
-        <p>Tarea Realizada</p>
+import "./task.css";
+export function TaskCard() {
+  const CardStyle = { background: "#202020", color: "white", padding: "20px" };
+  return (
+    <div className="card">
+      {/* Estilos en linea */}
+      <h1 style={{ fontWeight: "lighter" }}>Mi primer tarea</h1>
+      <p>Tarea Realizada</p>
     </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,42 +3,13 @@ import ReactDOM from "react-dom/client"; //Necesito react dom desde lado del cli
 import { Greeting, UserCard } from "./Greeting"; //importando el componente
 import Product, { Navbar } from "./Product";
 import { Button } from "./Button";
-import { Task } from "./Task";
+import { TaskCard } from "./Task";
 
 const rootElement = document.getElementById("root"); // obtengo el elemento "id:root" del index.html
 const root = ReactDOM.createRoot(rootElement); // Es un elemento que React lo usa bastante es el elemento raiz
 
 root.render(
   <div>
-    <Button text="click me" />
-    <Button text="Don't Click me" />
-    <Button text="" category="Action" />
-
-    {/* <UserCard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () {
-        alert("Bienvenido");
-      }}
-    />
-
-    <UserCard
-      name="Steven Calderon"
-      amount={0.01}
-      married={false}
-      points={[10, 20]}
-      address={{ street: "123 Per Street", city: "India York" }}
-    />
-
-    <UserCard
-      name="Katherin Monago"
-      amount={200000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Inca Street", city: "Pe York" }}
-    /> */}
+    <TaskCard/>
   </div>
 );
