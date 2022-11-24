6eef3948e51ab26bdc582a6a91ac377a9635d677
Steven Calderon <steven_cratus@hotmail.com>
2022-11-23 22:35:45 -0500

    React Props, What parameters can I use? Excercise UserCard

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,8 +1,30 @@
-export function Greeting({title=" Hola ",name="Anonimo"}) {
-  console.log(name)
-  return <h1>{title}, dice {name}</h1>;
+export function Greeting({ title = " Hola ", name = "Anonimo" }) {
+  console.log(name);
+  return (
+    <h1>
+      {title}, dice {name}
+    </h1>
+  );
 }
 
-export function Goodbye() {
-    return <h2> Chao Amigos</h2>
+function Goodbye() {
+  return <h2> Chao Amigos</h2>;
+}
+
+export function UserCard({ name, amount, points, married, address, greet }) {
+  console.log(greet)
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2> UserCard </h2>
+      <p>Amount: {amount}💵</p>
+      <p>Points: {points[1]}</p>
+      <p>State: {married ? "Married" : "Single"} </p>
+      <p>Address</p>
+      <ul>
+        <li>City: {address.city}</li>
+        <li>Street: {address.street}</li>
+      </ul>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; //Necesito react dom desde lado del cliente.
-import { Greeting } from "./Greeting"; //importando el componente
+import { Greeting, UserCard } from "./Greeting"; //importando el componente
 import Product, { Navbar } from "./Product";
 
 const rootElement = document.getElementById("root"); // obtengo el elemento "id:root" del index.html
@@ -8,11 +8,31 @@ const root = ReactDOM.createRoot(rootElement); // Es un elemento que React lo us
 
 root.render(
   <div>
-    <Greeting title="hola mundo" name="juan"/> {/* Mas actual y comun */}
-    <Greeting />
-    <Greeting />
-    <Greeting />
-    <Product></Product>
-    <Navbar></Navbar>
-  </div> 
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () {
+        alert("Bienvenido");
+      }}
+    />
+
+    <UserCard
+      name="Steven Calderon"
+      amount={0.01}
+      married={false}
+      points={[10, 20]}
+      address={{ street: "123 Per Street", city: "India York" }}
+    />
+
+    <UserCard
+      name="Katherin Monago"
+      amount={200000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Inca Street", city: "Pe York" }}
+    />
+  </div>
 );
