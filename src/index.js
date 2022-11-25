1eb00ac0d8bcb1298ed2222a6bb5d943cf82a7d3
Steven Calderon <steven_cratus@hotmail.com>
2022-11-24 23:30:08 -0500

    ReactHooks UseState, Exercise Basic

--- src/index.js
+++ src/index.js
@@ -1,32 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client"; //Necesito react dom desde lado del cliente.
-import { Greeting, UserCard } from "./Greeting"; //importando el componente
-import Product, { Navbar } from "./Product";
-import { Button } from "./Button";
-import { TaskCard } from "./Task";
-import { Posts } from "./Posts";
 
 const rootElement = document.getElementById("root"); // obtengo el elemento "id:root" del index.html
 const root = ReactDOM.createRoot(rootElement); // Es un elemento que React lo usa bastante es el elemento raiz
 
-const users = [
-  { id: 1, name: "Ryan Ray", image: "https://robohash.org/user1" },
-  { id: 2, name: "Joe", image: "https://robohash.org/user2" },
-  { id: 3, name: "Stevn", image: "https://robohash.org/user3" },
-  { id: 4, name: "David", image: "https://robohash.org/user4" },
-];
+function Counter() {
+  const [counter, setCounter] = useState(0);
+  const [message, setMessage] = useState("");
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Sumar
+      </button>
+
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        Restar
+      </button>
+
+      <button
+        onClick={() => {
+          setCounter(1000);
+        }}
+      >
+        Resetear
+      </button>
+
+      <input
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      ></input>
+
+      <button
+        onClick={() => {
+          alert(message);
+        }}
+      >
+        Alert
+      </button>
+    </>
+  );
+}
 
 root.render(
   <>
-    {users.map((user, index) => {
-      return (
-        <div key={index}>
-          <p>{user.name}</p>
-          <img src={user.image} />
-        </div>
-      );
-    })}
-
-    <p></p>
+    <Counter />
   </>
 );
